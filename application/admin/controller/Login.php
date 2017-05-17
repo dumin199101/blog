f655f473162fa3755efc4b4469deed6fa3d3c55f
dumin199101 <dumin199101@163.com>
2017-05-17 22:19:47 +0800

    lieyan#登录验证模型验证器#170517

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -2,6 +2,9 @@
 
 namespace app\admin\controller;
 
+use app\common\model\Admin;
+
+use houdunwang\crypt\Crypt;
 use think\Controller;
 
 class Login extends Controller
@@ -12,8 +15,22 @@ class Login extends Controller
      */
     public function index(){
         //测试数据库连接
-        $data = db('admin')->find(1);
-        dump($data);
-        return $this->fetch('login');
+//        $data = db('admin')->find(1);
+//        dump($data);
+        if(request()->isPost()){
+//            echo Crypt::decrypt('S4cj77RLvgCEeGK2WujphQ==');
+//            echo Crypt::encrypt('123456');
+//            die;
+            $res = (new Admin())->login(input('post.'));
+            if($res['valid']){
+                //登录成功：
+                $this->success($res['msg'],'admin/entry/index');
+            }else{
+                //登录失败：
+                $this->error($res['msg']);
+            }
+        }else{
+            return $this->fetch('login');
+        }
     }
 }
--- application/admin/validate/Admin.php
+++ application/admin/validate/Admin.php
@@ -9,7 +9,21 @@
 namespace app\admin\validate;
 
 
-class Admin
-{
+use think\Validate;
 
+class Admin extends  Validate
+{
+    //验证规则
+    protected $rule = [
+        'v_username'=>'require',
+        'v_password'=>'require',
+        'code'=>'require|captcha'
+    ];
+    //提示信息
+    protected $message = [
+        'v_username.require'=>'用户名不能为空',
+        'v_password.require'=>'密码不能为空',
+        'code.require'=>'验证码不能为空',
+        'code.captcha'=>'验证码不正确'
+    ];
 }
