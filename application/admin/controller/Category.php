46cf539af58cd0fa95728e76abff1523d903beb6
dumin199101 <dumin199101@163.com>
2017-05-20 22:09:45 +0800

    lieyan#栏目管理:首页数据获取及子栏目添加#170520

--- application/admin/controller/Category.php
+++ application/admin/controller/Category.php
@@ -16,6 +16,9 @@ class Category extends Controller
 
     //栏目管理
     public function index(){
+        $category_list = db('category')->select();
+//        halt($category_list);
+        $this->assign('category_list',$category_list);
         return $this->fetch();
     }
 
@@ -31,6 +34,25 @@ class Category extends Controller
              }
         }else{
             return $this->fetch();
+        }
+    }
+
+    //添加子栏目
+    public function addSon()
+    {
+        if(request()->isPost()){
+            $res = $this->db->store(input('post.'));
+            if($res['valid']!=0){
+                $this->success($res['msg'],'index');
+            }else{
+                $this->error($res['msg']);
+            }
+        }else {
+            //pathinfo模式下获取get参数使用的是param方式
+            $cate_id = input('param.cate_id');
+            $data = db('category')->where('n_id', $cate_id)->find();
+            $this->assign('data', $data);
+            return $this->fetch('addSon');
         }
     }
 }
--- application/admin/validate/Category.php
+++ application/admin/validate/Category.php
@@ -15,15 +15,15 @@ class Category extends Validate
 {
     protected $rule = [
         'v_cat_name'=>'require',
-        'n_pid'=>'require|number|between:0,9999',
-        'n_sort'=>'require|number'
+        'n_pid'=>'require|number',
+        'n_sort'=>'require|number|between:0,9999'
     ];
 
     protected $message = [
         'v_cat_name.require'=>'栏目名称不能为空',
         'n_pid.require'=>'所属栏目不能为空',
         'n_pid.number'=>'所属栏目必须是数字',
-        'n_pid.between'=>'所属栏目必须在1-9999之间',
+        'n_sort.between'=>'栏目排序必须在0-9999之间',
         'n_sort.require'=>'栏目排序不能为空',
         'n_sort.number'=>'栏目排序必须是数字',
     ];
