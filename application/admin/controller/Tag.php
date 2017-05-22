3e3ae2d80539ea2b7f1ca42363ac94be4f4be573
dumin199101 <dumin199101@163.com>
2017-05-22 23:12:56 +0800

    lieyan#标签管理:标签添加:控制器+模型#170522

---
+++ application/admin/controller/Tag.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\admin\controller;
+
+use think\Controller;
+
+class Tag extends Controller
+{
+    //
+    public function index()
+    {
+        return $this->fetch();
+    }
+
+    public function store()
+    {
+        return $this->fetch();
+    }
+}
---
+++ application/common/model/Tag.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\common\model;
+
+use think\Model;
+
+class Tag extends Model
+{
+    protected $pk = 'n_id';
+    protected $table  = 'tb_tag';
+
+}
