fcb46b86c779c3750dc610e689edca860baea6ab
dumin199101 <dumin199101@163.com>
2017-06-02 22:35:59 +0800

    lieyan#文章标签关联表数据添加#170602

--- application/common/model/Article.php
+++ application/common/model/Article.php
@@ -25,10 +25,21 @@ class Article extends Model
     //添加文章
     public function store($data){
         //验证器验证
-
+        if(empty($data['tag'])){
+            return['valid'=>0,'msg'=>'请选择标签'];
+        }
         //添加
-        $result = $this->validate(true)->save($data);
+        //过滤post数组中的非数据表字段数据
+        $result = $this->validate(true)->allowField(true)->save($data);
         if($result){
+            //文章标签中间表添加：
+            foreach($data['tag'] as $v){
+                $tag_data = [
+                    'n_article_id'=>$this->n_id, //获取自增长ID
+                    'n_tag_id'=>$v
+                ];
+                (new ArticleTag())->save($tag_data);
+            }
             return ['valid'=>1,'msg'=>'发布成功'];
         }else{
             return ['valid'=>0,'msg'=>$this->getError()];
---
+++ application/common/model/ArticleTag.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\common\model;
+
+use think\Model;
+
+class ArticleTag extends Model
+{
+    protected $pk = 'n_id';
+    protected $table = 'tb_article_tag';
+}
